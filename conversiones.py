804d69b9f9fe02ca95f4a08e51eaed3beb639b1e
EdoRodriguezP <ponce1604@gmail.com>
2025-05-28 21:02:12 -0400

    Se termina trabajo en desafios

--- conversiones.py
+++ conversiones.py
@@ -1,36 +1,39 @@
 import sys
 
-def validar_argumentos():  
+def validar_argumentos():       # Funcion para validar ingreso de 4 argumentos sino imprime y cierra programa
     if len(sys.argv) != 5:
         print("Error: Debe ingresar 4 argumentos")
         print("Uso: python conversiones.py [sol] [peso_arg] [dolar] [Monto_peso_chileno]")
-        sys.exit(1)
+        sys.exit(1)         # Termina con codigo de error.
         
-def conversion_monedas(monto, tasas):
-    conversiones = {
+def conversion_monedas(monto, tasas):     # Funcion para la conversion de monedas
+    conversiones = {                      # Diccionario con las conversiones multiplica el monto por cada tasa
         "soles": monto * tasas["sol"],
         "peso_arg": monto * tasas["peso_arg"],
         "dolar": monto * tasas["dolar"]
     }
-    return conversiones
+    return conversiones            # Retorna Diccionario conversiones con calculos ya realizados.
 
-def ingreso_tasas_monto():
-    validar_argumentos()
+def ingreso_tasas_monto():   # Funcion para ingreso y muestra de resultados
+    validar_argumentos()     # Se verifican argumentos
     try:
-        tasas = {
+        tasas = {                # Convierte los argumentos a números flotantes y los almacena en un diccionario
             "sol": float(sys.argv[1]),
             "peso_arg": float(sys.argv[2]),
             "dolar": float(sys.argv[3])
         }
-        monto_clp = float(sys.argv[4])
-        resultado = conversion_monedas(monto_clp, tasas)
-        print(f"Los {monto_clp:.0f} pesos equivalen a:")
+        monto_clp = float(sys.argv[4])    # Convierte monto a flotante
+        
+        resultado = conversion_monedas(monto_clp, tasas) # Llama a la variable conversion y la guarda en resultados
+        
+        print(f"Los {monto_clp:.0f} pesos equivalen a:")   # Imprime los resultados
         print(f"{resultado['soles']:.1f} Soles")
         print(f"{resultado['peso_arg']:.1f} Pesos Argentinos")
         print(f"{resultado['dolar']:.1f} Dólares")
-    except ValueError:
+        
+    except ValueError:        # Maneja error si los argumentos no son numeros validos
         print("Error: Todos los argumentos deben ser números")
         sys.exit(1)
 
-if __name__ == "__main__":
-    ingreso_tasas_monto()
+if __name__ == "__main__":    # Para que este código solo se ejecute cuando el script es el programa principal
+    ingreso_tasas_monto()     # Llama a la función para ejecutar
--- recordatorios.py
+++ recordatorios.py
@@ -4,24 +4,21 @@ recordatorios = [['2021-01-01', "11:00", "Levantarse y ejercitar"],
  ['2021-09-18', "16:00", "Ramadas"],
  ['2021-12-25', "00:00", "Navidad"]]
 
-# Output
-print("\nLista inicial de recordatorios:")
-for evento in recordatorios:
-    print(f"Fecha: {evento[0]}, Hora: {evento[1]}, Evento: {evento[2]}")
 
-recordatorios.append(['2021-02-02', "06:00", "Empezar el Año"])
-recordatorios.sort()
+recordatorios.append(['2021-02-02', "06:00", "Empezar el Año"])  # Se agrega nuevo recordatorio a la lista
 
-for evento in recordatorios:
+for evento in recordatorios:                # Bucle para buscar y modificar la fecha
     if evento[0] == '2021-07-15':
         evento[0] = '2021-07-16'
         
-recordatorios = [evento for evento in recordatorios if evento[2] != '2021-05-01']
-        
-recordatorios.append(['2021-12-24', "22:00", "Cena Navidad"])
-recordatorios.append(['2021-12-31', "22:00", "Cena Año Nuevo"])
-recordatorios.sort()
 
-print("\nLista final de recordatorios:")
+recordatorios = [x for x in recordatorios if x[0] != '2021-05-01']  # Crea una lista sin el elemento que coincida
+        
+        
+recordatorios.append(['2021-12-24', "22:00", "Cena Navidad"])       # Se agregan nuevos eventos al calendario
+recordatorios.append(['2021-12-31', "22:00", "Cena Año Nuevo"])
+recordatorios.sort()                     # Se reordena lista en orden ascendente
+
+print("\nLista final de recordatorios:")      # Bucle para imprimir en orden         
 for evento in recordatorios:
     print(f"Fecha: {evento[0]}, Hora: {evento[1]}, Evento: {evento[2]}")
